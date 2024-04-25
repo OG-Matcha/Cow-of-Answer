506baac98c5f2999a0f0f729ce65cd01d8f4a37b
黃鉅燊 <matchaskill@gmail.com>
2024-04-25 22:44:48 +0800

    add: add prettier and eslint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,25 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  experimental: { sharedPrerenderData: true },
-  ssr: false,
-  devtools: { enabled: false },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  vite: {
-    server: {
-      watch: {
-        usePolling: true, 
+    experimental: { sharedPrerenderData: true },
+    ssr: false,
+    devtools: { enabled: false },
+    modules: [
+      '@nuxtjs/tailwindcss'
+    ],
+    vite: {
+      server: {
+        watch: {
+          usePolling: true, 
+        },
       },
     },
-  },
-  runtimeConfig: {
-    // Keys within public, will be also exposed to the client-side
-    public: {
-      apiURL: 'http://localhost:8000'
-    }
-  },
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
+    app: {
+        pageTransition: { name: 'slide', mode: 'out-in' }
+    },
 })
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,33 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    `./components/**/*.{vue,js,ts}`,
-    `./layouts/**/*.vue`,
-    `./pages/**/*.vue`,
-    `./plugins/**/*.{js,ts}`,
-    `./utils/**/*.{js,ts}`,
-    `./App.{js,ts,vue}`,
-    `./app.{js,ts,vue}`,
-    `./Error.{js,ts,vue}`,
-    `./error.{js,ts,vue}`,
-    `./app.config.{js,ts}`
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#6750A4',
-        'secondary': '#D0BCFF',
-        'secondary2': '#E8DEF8',
-        'terotory': '#A39292',
-        'bgcolor': '#FFFBFF',
-        'error': '#FFF0F0',
-        'hovercolor': '#E8DEF8',
+module.exports = {
+    content: [
+      './components/**/*.{vue,js,ts}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './composables/**/*.{js,ts}',
+      './plugins/**/*.{js,ts}',
+      './app.{js,ts,vue}'
+    ],
+    theme: {
+        extend: {
+          colors: {
+          }
+        },
       },
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        chen: ['ChenYuluoyan-Thin-Monospaced', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-}
+    plugins: []
+  }
