19c9b34192277fae76c712c9d202bdaae7060815
a73254310 <a73254310@gmail.com>
2024-05-02 00:41:24 +0800

    add challenge page

---
+++ frontend/pages/BookPage/index.vue
@@ -0,0 +1,6 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+<template>
+  <div class="bg-table h-[100%] w-[100%]">jij</div>
+</template>
---
+++ frontend/pages/InsideChallenge/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+<template>
+  <div class="bg-table h-[100%] w-[100%]">
+    <img src="/Grassbackground.png" alt="background" class="h-auto w-full" />
+    <div class="bg-table h-[11%] w-full"></div>
+    <div class="bg-table h-[89%] w-full"></div>
+  </div>
+</template>
